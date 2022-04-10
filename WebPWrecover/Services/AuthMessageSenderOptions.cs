09dc9a9da8f72bf3ed03ffdb1c0d3e09b0299186
Steve Linz <steve.linz@gmail.com>
2022-04-10 10:06:57 -0500

    first commit

---
+++ WebPWrecover/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPWrecover.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string? SendGridKey {get; set;}
+    }
+}
